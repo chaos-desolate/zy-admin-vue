176d25de6bf06a9764793af785a3d7654efdf567
chaos <zane_zeus@foxmail.com>
2024-10-10 14:24:32 +0800

    feat: 导航菜单

---
+++ src/views/login/ZSubmenu.vue
@@ -0,0 +1,30 @@
+<template>
+    <template
+        v-for="item in props.menuItems"
+        :key="item.path">
+        <a-sub-menu
+            v-if="item.children"
+            :key="item.path">
+            <template #title>
+                <span>{{ item.meta.title }}</span>
+            </template>
+            <z-submenu :menu-items="item.children"></z-submenu>
+        </a-sub-menu>
+        <a-menu-item
+            v-else
+            :key="item.path">
+            {{ item.meta.title }}
+        </a-menu-item>
+    </template>
+</template>
+
+<script setup name="ZSubmenu">
+const props = defineProps({
+    menuItems: {
+        type: Array,
+        required: true,
+    },
+})
+</script>
+
+<style scoped lang="less"></style>
