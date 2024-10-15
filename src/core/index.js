a43cd1587ce84233c9401d789647d4e7a544b8e0
chaos <zane_zeus@foxmail.com>
2024-10-15 11:45:52 +0800

    feat: 引入tailwind

--- src/core/index.js
+++ src/core/index.js
@@ -2,6 +2,7 @@ import antd from 'ant-design-vue'
 import { setupRouter } from '@/routers'
 import { setupStore } from '@/store'
 import 'ant-design-vue/dist/reset.css'
+import '@/styles/index.less'
 
 export const setupCores = (app) => {
     app.use(antd)
